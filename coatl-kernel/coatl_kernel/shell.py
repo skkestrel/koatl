a6bf92bf71b665437edcc531250356c168cee510
KZ <kevin@kouver.ca>
2025-07-18 12:39:37 -0400

    override error reporting in kernel

--- coatl-kernel/coatl_kernel/shell.py
+++ coatl-kernel/coatl_kernel/shell.py
@@ -8,10 +8,27 @@ from ipykernel.compiler import XCachingCompiler
 from ipykernel.zmqshell import ZMQInteractiveShell
 from IPython.core.error import InputRejected
 from IPython.core.interactiveshell import *
+from IPython.core.ultratb import FrameInfo
+
+_source_map_override = {}
+_orig_frame_info_init = None
+
+if _orig_frame_info_init is None:
+    _orig_frame_info_init = FrameInfo.__init__
+
+    def _new_init(self, *args, **kwargs):
+        _orig_frame_info_init(self, *args, **kwargs)
+
+        # only override for ipykernel frames
+        if "ipykernel_" in self.filename:
+            old_lineno = self.lineno
+            self.lineno = _source_map_override.get(old_lineno, 0)
+            self._sd.lineno = _source_map_override.get(old_lineno, 0)
+
+    FrameInfo.__init__ = _new_init
 
 class CoatlXCachingCompiler(XCachingCompiler):
     def cache(self, transformed_code, number=0, raw_code=None):
-        print(self.shell._coatl_cell)
         return super().cache(self.shell._coatl_cell, number, raw_code)
 
 
@@ -23,16 +40,22 @@ class CoatlShell(ZMQInteractiveShell):
 
     def _get_exc_info(self, exc_tuple=None):
         ret = super()._get_exc_info(exc_tuple)
-        print(ret)
+        self._exc_info = ret
         return ret
 
     def transform_cell(self, raw_cell):
         cell = super().transform_cell(raw_cell)
 
         self._coatl_cell = cell
-        (py_ast, sourcemap) = coatl.transpile(cell, mode="interactive", sourcemap=True)
-        self._coatl_sourcemap = sourcemap
+        (cell, sourcemap) = coatl.transpile(cell, mode="interactive", sourcemap=True)
+        _source_map_override.clear()
 
-        cell = ast.unparse(py_ast)
+        last_seen_line = 0
+        for i in range(len(cell.splitlines()) + 1):
+            if i in sourcemap:
+                _source_map_override[i] = sourcemap[i]
+                last_seen_line = sourcemap[i]
+            else:
+                _source_map_override[i] = last_seen_line
 
         return cell
