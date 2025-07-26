7cc729ae891467176e596b82f90d910a701bd2d4
KZ <raymarching@gmail.com>
2025-07-26 16:51:47 -0400

    clean up async

--- koatl/python/koatl/prelude/functional/async_util.py
+++ koatl/python/koatl/prelude/functional/async_util.py
@@ -1,29 +1,15 @@
 """
 At the time of writing, I haven't implemented async def
-in Koatl, so this is a workaround to provide implementations
-for Async.
+in Koatl, so this is a workaround.
+
+This function converts a generic awaitable (perhaps a
+vanilla generator) into a coroutine that can be used
+by asyncio.run, which expects strictly a coroutine.
 
 (In practice, async def shouldn't be needed anyways,
 since we have the Async monad.)
 """
 
 
-async def await_one(awaitable):
+async def to_coro(awaitable):
     return await awaitable
-
-
-async def defer_await(fn, *args, **kwargs):
-    return await fn(*args, **kwargs)
-
-
-async def bind_async(awaitable, func):
-    result = func(await awaitable)
-
-    if hasattr(result, "__await__"):
-        return await result
-
-    return result
-
-
-async def pure(value):
-    return value
