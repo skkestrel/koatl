82fafb288257b3f5f6963b0f6e04f2ea619279db
KZ <raymarching@gmail.com>
2025-09-01 12:51:32 -0400

    stuff

--- quetzal/src/extension.ts
+++ quetzal/src/extension.ts
@@ -4,10 +4,7 @@ import { WasmContext, Memory } from "@vscode/wasm-component-model";
 import { formatter } from "./formatter";
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
-	const channel = vscode.window.createOutputChannel("Calculator");
-	context.subscriptions.push(channel);
-
-	const log = vscode.window.createOutputChannel("Calculator - Log", { log: true });
+	const log = vscode.window.createOutputChannel("Quetzal", { log: true });
 	context.subscriptions.push(log);
 
 	// Load the Wasm module
@@ -32,26 +29,10 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 	// Bind the TypeScript Api
 	const api = formatter._.exports.bind(instance.exports as formatter._.Exports, wasmContext);
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand("koatl.format", () => {
-			channel.show();
-			channel.appendLine("Running formatter example");
-			channel.appendLine(`Add ${api.format("  let x = 1 + 2;   ")}`);
-		}),
-	);
-
-	channel.show();
-	channel.appendLine("Activating");
-
 	vscode.languages.registerDocumentFormattingEditProvider("koatl", {
 		provideDocumentFormattingEdits(document) {
-			channel.show();
-			channel.appendLine("Running formatter");
-
 			const doc = document.getText();
 			const formatted = api.format(doc);
-
-			channel.appendLine(formatted);
 
 			return [vscode.TextEdit.replace(new vscode.Range(0, 0, document.lineCount, 0), formatted)];
 		},
