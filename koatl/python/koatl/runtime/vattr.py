1eed235ee4c97b42b9d95e05cc0e2bf8677cbf1d
KZ <raymarching@gmail.com>
2025-11-21 18:31:46 -0500

    remove top 2 frames frames in traceback on vget error

--- koatl/python/koatl/runtime/vattr.py
+++ koatl/python/koatl/runtime/vattr.py
@@ -7,37 +7,36 @@ def vget(obj, name, ignore_traits=False):
     try:
         return getattr(obj, name)
     except AttributeError as e:
-        orig_err = e
+        # special case for iter - this could be implemented using types and trait vtbls
+        # but this is simpler and probably faster
+        if name == "iter":
+            if isinstance(obj, slice):
+                start = obj.start if obj.start is not None else 0
+                step = obj.step if obj.step is not None else 1
+                if obj.stop is None:
+                    return iter(count(start, step))
+                else:
+                    return iter(range(start, obj.stop, step))
 
-    # special case for iter - this could be implemented using types and trait vtbls
-    # but this is simpler and probably faster
-    if name == "iter":
-        if isinstance(obj, slice):
-            start = obj.start if obj.start is not None else 0
-            step = obj.step if obj.step is not None else 1
-            if obj.stop is None:
-                return iter(count(start, step))
-            else:
-                return iter(range(start, obj.stop, step))
+            try:
+                return iter(obj.items())
+            except AttributeError:
+                pass
 
-        try:
-            return iter(obj.items())
-        except AttributeError:
-            pass
+            try:
+                return iter(obj)
+            except TypeError:
+                pass
 
-        try:
-            return iter(obj)
-        except TypeError:
-            pass
+        v = fast_vget(obj, name, ignore_traits)
+        if v is not None:
+            if hasattr(v, "_property"):
+                return v(obj)
 
-    v = fast_vget(obj, name, ignore_traits)
-    if v is not None:
-        if hasattr(v, "_property"):
-            return v(obj)
+            return partial(v, obj)
 
-        return partial(v, obj)
-
-    raise orig_err
+        e.__traceback__ = None
+        raise
 
 
 def vhas(obj, name, ignore_traits=False):
